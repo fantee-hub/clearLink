fc308d364d6756e5317067b164820d7d69a84a88
Tobi Faniran <tobifaniran@gmail.com>
2023-10-12 21:35:34 +0100

    feat: added new assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import ClearlinkAdvantage from "@/src/Components/ClearlinkAdvantage";
 import Hero from "@/src/Components/Hero";
 import Sponsors from "@/src/Components/Sponsors";
+import Testimonies from "@/src/Components/Testimonies";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Sponsors />
       <ClearlinkAdvantage />
+      <Testimonies />
     </main>
   );
 }
---
+++ src/Components/Testimonies/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testimonies = () => {
+  return (
+    <section className="max-w-[1472px] 2xl:mx-auto px-10 mx-auto relative py-[96px]">
+      Testimonies
+    </section>
+  );
+};
+
+export default Testimonies;
