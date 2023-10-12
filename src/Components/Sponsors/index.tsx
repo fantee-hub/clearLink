6500a1f3ec734a1d781f0e51e7709e4d93393667
Tobi Faniran <tobifaniran@gmail.com>
2023-10-12 20:53:36 +0100

    feat: companies section

--- src/Components/Sponsors/index.tsx
+++ src/Components/Sponsors/index.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Sponsors = () => {
-  return <div className="bg-white">Sponsors</div>;
+  return (
+    <section className="max-w-[1472px] 2xl:mx-auto px-10 mx-auto relative py-[96px]">
+      <div className="text-center text-slate-600 text-xl font-medium">
+        Join 1,500+ companies already video conferencing the ClearLink way
+      </div>
+      <div className="flex justify-center gap-[69px] flex-wrap items-center pt-12">
+        <div>
+          <img src="/assets/images/companies/Shopify.png" alt="shopify" />
+        </div>
+        <div>
+          <img src="/assets/images/companies/Coinbase.png" alt="coinbase" />
+        </div>
+        <div>
+          <img src="/assets/images/companies/Dropbox.png" alt="drop box" />
+        </div>
+        <div>
+          <img src="/assets/images/companies/Intercom.png" alt="intercom" />
+        </div>
+        <div>
+          <img src="/assets/images/companies/Marvel.png" alt="marvel" />
+        </div>
+        <div>
+          <img src="/assets/images/companies/Automattic.png" alt="automattic" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Sponsors;
