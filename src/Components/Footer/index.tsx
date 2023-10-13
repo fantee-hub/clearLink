fc62bc62262540017b791fb459433cbe6e39d7ea
Tobi Faniran <tobifaniran@gmail.com>
2023-10-13 16:25:34 +0100

    feat: added new assets

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -3,7 +3,61 @@ import React from "react";
 const Footer = () => {
   return (
     <section className="max-w-[1472px] 2xl:mx-auto px-10 mx-auto relative py-[96px]">
-      Footer
+      <div>
+        <div>
+          <div>
+            <span>
+              <img src="/assets/logo/logo.svg" alt="logo" />
+            </span>
+            <span>ClearLink.</span>
+          </div>
+          <div>
+            ClearLink is your gateway to effortless, high-quality video
+            conferencing. Join us in shaping the future of communication!
+          </div>
+        </div>
+        <div>
+          <div>Product</div>
+          <ul>
+            <li>Overview</li>
+            <li>Features</li>
+            <li>Solutions</li>
+            <li>Tutorials</li>
+            <li>Pricing</li>
+          </ul>
+        </div>
+        <div>
+          <div>Company</div>
+          <ul>
+            <li>About us</li>
+            <li>Careers</li>
+            <li>Press</li>
+            <li>News</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <div>Resources</div>
+          <ul>
+            <li>Blog</li>
+            <li>Events</li>
+            <li>Help Centre</li>
+            <li>Tutorials</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div>
+          <div>Legal</div>
+          <ul>
+            <li>Terms</li>
+            <li>Privacy</li>
+            <li>Cookies</li>
+            <li>Licences</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
     </section>
   );
 };
