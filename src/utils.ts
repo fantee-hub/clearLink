ea05607ce8a7cb6dce5a0e3f7d1c05e5352bc8be
Tobi Faniran <tobifaniran@gmail.com>
2023-10-13 13:57:09 +0100

    added a utils file

---
+++ src/utils.ts
@@ -0,0 +1,38 @@
+export const faqs = [
+  {
+    id: crypto.randomUUID(),
+    title: "How many participants can join a ClearLink video conference?",
+    answer:
+      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Can I use ClearLink on multiple devices?",
+    answer:
+      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Is ClearLink compatible with other video conferencing platforms?",
+    answer:
+      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "How does ClearLink ensure the security of my video conferences?",
+    answer:
+      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Do I need to download any software to use ClearLink?",
+    answer:
+      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "What kind of customer support does ClearLink provide?",
+    answer:
+      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+  },
+];
